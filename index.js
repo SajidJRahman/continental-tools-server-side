6ae436eca5cad74723ca8ffc1124760abd903634
Sajid Rahman <sajidjrahman@icloud.com>
2022-05-23 04:04:00 +0200

    added products API

--- index.js
+++ index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4cpv5.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+const run = async () => {
+    try {
+        await client.connect();
+        const collectionDB = client.db("continentalTools").collection("products");
+
+        app.get('/products', async (req, res) => {
+            const query = {};
+            const cursor = collectionDB.find(query);
+            const result = await cursor.toArray(cursor);
+            res.send(result);
+        });
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
     res.send('Continental Tools');
