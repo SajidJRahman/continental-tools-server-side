48063483befec7810ee1eff611a266260a39290d
Sajid Rahman <sajidjrahman@icloud.com>
2022-05-28 23:38:12 +0200

    still trying to fix cors policy error

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 5000;
 const corsConfig = {
     origin: '*',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    methods: ['GET', 'POST', 'PUT', 'DELETE']
 }
 app.use(cors(corsConfig))
 app.options("*",
