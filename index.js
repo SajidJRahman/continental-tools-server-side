bef75d9e7c55bc45459ae12bf8d01a192f5c4cc3
Sajid Rahman <sajidjrahman@icloud.com>
2022-05-28 22:42:54 +0200

    trying to add fix for cors policy error

--- index.js
+++ index.js
@@ -6,7 +6,13 @@ require('dotenv').config();
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const app = express();
 const port = process.env.PORT || 5000;
-app.use(cors());
+app.use(
+    cors({
+        origin: true,
+        optionsSuccessStatus: 200,
+        credentials: true,
+    })
+);
 app.use(express.json());
 
 // URI & CLIENT
